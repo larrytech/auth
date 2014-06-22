9faa3a853880dd7adc8800993a5d03de36a65cfe
George Robinson <george.robinson@larrytech.com>
2014-06-22 20:23:20 +0000

    Remove Parentheses After Throws Exception

--- src/models/User.php
+++ src/models/User.php
@@ -56,7 +56,7 @@ class User extends Model implements UserInterface, RemindableInterface {
 
         if ($this->hasRole($role))
         {
-            throw new DuplicateRoleException();
+            throw new DuplicateRoleException;
         }
 
         if ($role instanceof Role)
